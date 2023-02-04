cd04d907027d5f50eacfd65901bc21384cc1d20b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-04 19:31:25 +0530

    :hammer: chore: updating commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['./node_modules/commitlint-config-gitmoji'],
   rules: {
-    'scope-enum': [2, 'always', ['global', 'akagi', 'kaguya', 'globalkey']],
+    'scope-enum': [2, 'always', ['global', 'client', 'server', 'globalkey']],
   },
 };
