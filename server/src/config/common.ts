22968b29d4a893d6c92ece4a983986f561cee9ac
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-04 20:38:08 +0530

    :zap: chore: adding common server config

---
+++ server/src/config/common.ts
@@ -0,0 +1,15 @@
+import { __prod__ } from '../constants';
+import { getEnvVariable } from '../common/utils/env';
+
+export const config = {
+  appName: 'Reddit',
+  database: {
+    type: 'postgres',
+    url: getEnvVariable('DATABASE_URL'),
+    // url: __prod__ ? process.env.DATABASE_URL : undefined,
+    entities: ['dist/entities/*.js'],
+    migrations: ['dist/migrations/*.js'],
+    synchronize: !__prod__,
+    logging: !__prod__,
+  },
+};
