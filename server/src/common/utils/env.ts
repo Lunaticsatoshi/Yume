ea48a57ec47c05988640d5395044f4adc525a92b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-04 20:36:29 +0530

    :zap: chore: env methods

---
+++ server/src/common/utils/env.ts
@@ -0,0 +1,7 @@
+export const getEnvVariable = (variableName: string) => {
+  if (process.env[variableName]) {
+    return process.env[variableName];
+  }
+
+  throw new Error(`${variableName} not found in ENV`);
+};
