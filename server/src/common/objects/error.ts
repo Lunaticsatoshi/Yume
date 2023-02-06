f57267b7049d0554864abd40cf8dafe03b5d645e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-07 01:33:08 +0530

    :zap: chore: error response object

---
+++ server/src/common/objects/error.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class ErrorResponse {
+  @Field()
+  statusCode: string;
+
+  @Field({ nullable: true })
+  field: string;
+
+  @Field()
+  message: string;
+}
