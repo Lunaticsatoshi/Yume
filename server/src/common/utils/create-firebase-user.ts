018c4c2d2833b35e66083dbe2e3a7d0588d4a461
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-07 01:33:25 +0530

    :zap: chore: create firebase users

---
+++ server/src/common/utils/create-firebase-user.ts
@@ -0,0 +1,25 @@
+import admin from '../firebase';
+
+export const createFirebaseUser = async (email: string, userId: string) => {
+  try {
+    let user = null;
+
+    try {
+      user = await admin.auth().createUser({
+        email,
+      });
+    } catch (e) {
+      if (e.code === 'auth/email-already-exists') {
+        user = await admin.auth().getUserByEmail(email);
+      } else {
+        throw e;
+      }
+    }
+
+    await admin.auth().setCustomUserClaims(user.uid, {
+      id: userId,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
