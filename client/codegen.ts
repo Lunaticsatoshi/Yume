2d054fa5f6d337355e60391385c281b0e306a1ca
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-06 18:31:26 +0530

    :sparkles: chore: setting up codegen

---
+++ client/codegen.ts
@@ -0,0 +1,34 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+const dotenv = require('dotenv');
+const path = require('path');
+
+/**
+ * Load .env file or for tests the .env.test file.
+ */
+const dotenvFile = `.env.local`;
+dotenv.config({ path: path.join(process.cwd(), dotenvFile) });
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: process.env.NEXT_PUBLIC_API_URL,
+  documents: "src/graphql/**/*.graphql",
+  generates: {
+    "src/generated/graphql.tsx": {
+      preset: "client",
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        skipTypename: false,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    }
+  }
+};
+
+export default config;
