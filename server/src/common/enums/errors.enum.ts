cafbb052c75df5ce4ee5aff40be85878b90c12b1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-09 13:59:52 +0530

    :label: chore: adding error enums

---
+++ server/src/common/enums/errors.enum.ts
@@ -0,0 +1,9 @@
+export enum GeneralErrors {
+  SERVER = 'Error: something went wrong',
+}
+export enum UserErrors {
+  GetUser = 'Error: user not found',
+  GetUsers = 'Error: cannot get user list',
+  CreateUser = 'Error: unable to register user',
+  UpdateUser = 'Error: unable to update user',
+}
