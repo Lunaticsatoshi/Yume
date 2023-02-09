870d4ddd98d5fae9b69c9eaef841e3030daa33c0
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-09 14:00:48 +0530

    :zap: chore: updatig datasource initialization in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ config();
 
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { createDataSource } from './common/utils/data-source';
+import { createDataSource } from './common/utils/dataSource';
 import app from './app';
 import { UserResolver } from './modules/user/user-resolver';
 
@@ -11,15 +11,14 @@ const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || 'development';
 
 (async () => {
-  console.log("Connecting to Postgress.....");
+  console.log('Connecting to Postgress.....');
   try {
     const dataSource = await createDataSource();
 
-    await dataSource.initialize();
     await dataSource.synchronize();
     // await dataSource.runMigrations();
-    if(dataSource.isInitialized) {
-      console.log("connected to database");
+    if (dataSource.isInitialized) {
+      console.log('connected to database');
     }
 
     // console.log("connected, running migrations now", dataSource);
@@ -47,6 +46,6 @@ const ENV: string | number = process.env.NODE_ENV || 'development';
       console.log(`server started on port ${PORT} in ${ENV} mode`),
     );
   } catch (error) {
-    console.log("error", error.message);
+    console.log('error', error.message);
   }
 })();
