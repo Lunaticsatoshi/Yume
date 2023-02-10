e5f4b64215e0e4da06a9126922e78a92c4e57e63
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-10 13:09:28 +0530

    :lipstick: chore: layout styling fixes

--- client/src/components/shared/Layout.tsx
+++ client/src/components/shared/Layout.tsx
@@ -28,7 +28,7 @@ const Layout: FC<LayoutProps> = ({
       <AuthModalContextProvider>
         <div className="relative">
           <Navbar />
-          <div className="main-content flex justify-between h-full bg-gray-100 dark:bg-black-900">
+          <div className="main-content flex flex-col h-screen bg-gray-100 dark:bg-black-900">
             {children}
           </div>
         </div>
