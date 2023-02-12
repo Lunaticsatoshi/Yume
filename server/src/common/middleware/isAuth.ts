a57e07101172446dd5ed31480347bee68f21751d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-12 18:38:55 +0530

    :safety_vest: chore: updating auth middlewares

--- server/src/common/middleware/isAuth.ts
+++ server/src/common/middleware/isAuth.ts
@@ -1,10 +1,34 @@
 import { MiddlewareFn } from "type-graphql";
-import { RequestContext } from "../interfaces/RequestContext";
+
+import { RequestContext, IRequestContextUser } from "../interfaces/RequestContext";
 import { parseFirebaseToken } from "../utils/firebase-token-parser";
 
-// bearer 102930ajslkdaoq01
-
 export const isAuth: MiddlewareFn<RequestContext> = async ({ context }, next) => {
+  // eslint-disable-next-line dot-notation
+  const authorization = context.req.headers["authorization"] as string;
+  const mimicId = context.req.headers["x-mimic-user-id"] as string;
+
+  if (!authorization) {
+    context.user = {} as IRequestContextUser;
+
+    return next();
+  }
+
+  try {
+    const payload = await parseFirebaseToken(authorization, mimicId);
+
+    // eslint-disable-next-line require-atomic-updates
+    context.user = payload as any;
+  } catch (err) {
+    // eslint-disable-next-line require-atomic-updates
+    console.log(err);
+    throw new Error(err.message);
+  }
+
+  return next();
+};
+
+export const isStrictAuth: MiddlewareFn<RequestContext> = async ({ context }, next) => {
   // eslint-disable-next-line dot-notation
   const authorization = context.req.headers["authorization"] as string;
   const mimicId = context.req.headers["x-mimic-user-id"] as string;
@@ -20,7 +44,7 @@ export const isAuth: MiddlewareFn<RequestContext> = async ({ context }, next) =>
     context.user = payload as any;
   } catch (err) {
     console.log(err);
-    throw new Error("not authenticated");
+    throw new Error(err.message);
   }
 
   return next();
