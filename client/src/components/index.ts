6238f17258007ba3fd462a5c346b053563f4ef54
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-22 18:55:48 +0530

    :zap: chore: default imports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,17 +1,24 @@
 import { InputField } from './ui/form/InputField';
-import { ActionButton } from './ui/Button';
+import { ActionButton, JoinButton } from './ui/Button';
 import { GoogleOAuthButton } from './ui/OAuthButton';
 import AuthModal from './ui/modal/AuthModal';
 import CreateCommunityModal from './ui/modal/CreateCommunity';
 import SearchInput from './ui/SearchInput';
 import Layout from './shared/Layout';
 import PageContentLayout from './shared/PageContentLayout';
-import { DefaultCircularAvatar, CircularAvatar } from './ui/avatar/CircularAvatar';
+import { DefaultCircularAvatar, CircularAvatar, DefaultSmallCircularAvatar } from './ui/avatar/CircularAvatar';
 import HeaderBanner from './ui/banner/HeaderBanner';
+import AboutCommunity from './about/Community';
+import CreatePostLink from './post/CreatePost';
+import CommunityNotFound from './error/CommunityNotFound';
+import BaseError from './error/BaseError';
+import CreatePostForm from './post/CreatePostForm/CreatePostForm';
+
 
 export {
   InputField,
   ActionButton,
+  JoinButton,
   SearchInput,
   Layout,
   PageContentLayout,
@@ -19,6 +26,12 @@ export {
   AuthModal,
   CreateCommunityModal,
   DefaultCircularAvatar,
+  DefaultSmallCircularAvatar,
   CircularAvatar,
   HeaderBanner,
+  AboutCommunity,
+  CreatePostLink,
+  CommunityNotFound,
+  BaseError,
+  CreatePostForm
 };
