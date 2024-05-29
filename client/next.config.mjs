b53e4e52ac6cbce68563ff796129f68ee1875f69
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-05-29 20:43:55 +0530

    :wrench: chore: updating configs

---
+++ client/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
