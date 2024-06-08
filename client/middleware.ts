1706f60653ebdb860d60d159959979692e3ed48e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-06-08 16:58:48 +0530

    :sparkles: chore: adding support for supabase

---
+++ client/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { createClient } from '@/utils/supabase/middleware';
+
+export async function middleware(request: NextRequest) {
+  const { supabase, response } = createClient(request);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user && !request.nextUrl.pathname.startsWith('/login')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return response;
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
